cd4d91edc204187d49a5556d2295fb3172c679bc
Afaque Ahmed <105604620+Afaque22@users.noreply.github.com>
2024-08-06 15:36:06 +0500

    Update index.ts

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -15,6 +15,7 @@ export interface IUserStory<T = Record<string, any>> {
   stories: IUserStoryItem<T>[];
   /** INTERNAL USE ONLY */
   seen?: boolean;
+  isSeen? : boolean;
 }
 
 export interface IUserStoryItem<T = Record<string, any>> {
