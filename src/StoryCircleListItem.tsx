de7c663021d6385603c115edfc3d2ebe20a94de6
Afaque Ahmed <105604620+Afaque22@users.noreply.github.com>
2024-08-15 12:36:08 +0500

    Update StoryCircleListItem.tsx

--- src/StoryCircleListItem.tsx
+++ src/StoryCircleListItem.tsx
@@ -1,443 +1,137 @@
-//changes by Afaque
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
-  Animated,
-  Image,
-  Text,
-  TouchableOpacity,
-  StyleSheet,
-  Dimensions,
-  TouchableWithoutFeedback,
-  ActivityIndicator,
   View,
+  Image,
+  TouchableOpacity,
+  Text,
+  StyleSheet,
   Platform,
-  SafeAreaView,
 } from 'react-native';
-import GestureRecognizer from 'react-native-swipe-gestures';
 
-import { usePrevious, isNullOrWhitespace } from './helpers';
-import {
-  IUserStoryItem,
-  NextOrPrevious,
-  StoryListItemProps,
-} from './interfaces';
-import Video from 'react-native-video';
+import { usePrevious } from './helpers/StateHelpers';
+import { IUserStory, StoryCircleListItemProps } from './interfaces';
 
-const { width, height } = Dimensions.get('window');
+import DEFAULT_AVATAR from './assets/images/no_avatar.png';
+import { useGlobalChatContext } from '../../../context/ChatContext';
 
-export const StoryListItem = ({
-  index,
-  key,
-  userId,
-  profileImage,
-  profileName,
-  duration,
-  onFinish,
-  onClosePress,
-  stories,
-  currentPage,
-  onStorySeen,
-  renderCloseComponent,
-  renderSwipeUpComponent,
-  renderTextComponent,
-  loadedAnimationBarStyle,
-  unloadedAnimationBarStyle,
-  animationBarContainerStyle,
-  storyUserContainerStyle,
-  storyImageStyle,
-  storyAvatarImageStyle,
-  storyContainerStyle,
-  ...props
-}: StoryListItemProps) => {
-  const [load, setLoad] = useState<boolean>(true);
-  const [pressed, setPressed] = useState<boolean>(false);
-  const [content, setContent] = useState<IUserStoryItem[]>(
-    stories.map((x) => ({
-      ...x,
-      finish: 0,
-    })),
-  );
+const StoryCircleListItem = ({
+  item,
+  unPressedBorderColor,
+  pressedBorderColor,
+  unPressedAvatarTextColor,
+  pressedAvatarTextColor,
+  avatarSize = 60,
+  showText,
+  avatarTextStyle,
+  handleStoryItemPress,
+  avatarImageStyle,
+  avatarWrapperStyle,
+}: StoryCircleListItemProps) => {
+  const [isPressed, setIsPressed] = useState(item?.seen);
+  const {user} = useGlobalChatContext();
 
-  const [current, setCurrent] = useState(0);
-
-  const progress = useRef(new Animated.Value(0)).current;
-
-  const prevCurrentPage = usePrevious(currentPage);
+  const prevSeen = usePrevious(item?.seen);
 
   useEffect(() => {
-    let isPrevious = !!prevCurrentPage && prevCurrentPage > currentPage;
-    if (isPrevious) {
-      setCurrent(content.length - 1);
-    } else {
-      setCurrent(0);
-    }
 
-    let data = [...content];
-    data.map((x, i) => {
-      if (isPrevious) {
-        x.finish = 1;
-        if (i == content.length - 1) {
-          x.finish = 0;
-        }
-      } else {
-        x.finish = 0;
-      }
-    });
-    setContent(data);
-    start();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [currentPage]);
-
-  const prevCurrent = usePrevious(current);
-
-  useEffect(() => {
-    if (!isNullOrWhitespace(prevCurrent)) {
-      if (prevCurrent) {
-        if (
-          current > prevCurrent &&
-          content[current - 1].story_image == content[current].story_image
-        ) {
-          start();
-        } else if (
-          current < prevCurrent &&
-          content[current + 1].story_image == content[current].story_image
-        ) {
-          start();
-        }
-      }
+    if (prevSeen != item?.seen) {
+      setIsPressed(item?.seen);
+    
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [current]);
+  }, [item?.seen]);
 
-  function start() {
-    setLoad(false);
-    progress.setValue(0);
-    startAnimation();
-  }
+  const _handleItemPress = (item: IUserStory) => {
+    if (handleStoryItemPress) handleStoryItemPress(item);
 
-  function startAnimation() {
-    Animated.timing(progress, {
-      toValue: 1,
-      duration: duration,
-      useNativeDriver: false,
-    }).start(({ finished }) => {
-      if (finished) {
-        next();
-      }
-    });
-  }
-
-  function onSwipeUp(_props?: any) {
-    if (onClosePress) {
-      onClosePress();
-    }
-    if (content[current].onPress) {
-      content[current].onPress?.();
-    }
-  }
-
-  function onSwipeDown(_props?: any) {
-    onClosePress();
-  }
-
-  const config = {
-    velocityThreshold: 0.3,
-    directionalOffsetThreshold: 80,
+    setIsPressed(true);
   };
 
-  function next() {
-    // check if the next content is not empty
-    setLoad(true);
-    if (current !== content.length - 1) {
-      let data = [...content];
-      data[current].finish = 1;
-      setContent(data);
-      setCurrent(current + 1);
-      progress.setValue(0);
-    } else {
-      // the next content is empty
-      close('next');
-    }
-  }
-
-  function previous() {
-    // checking if the previous content is not empty
-    setLoad(true);
-    if (current - 1 >= 0) {
-      let data = [...content];
-      data[current].finish = 0;
-      setContent(data);
-      setCurrent(current - 1);
-      progress.setValue(0);
-    } else {
-      // the previous content is empty
-      close('previous');
-    }
-  }
-
-  function close(state: NextOrPrevious) {
-    let data = [...content];
-    data.map((x) => (x.finish = 0));
-    setContent(data);
-    progress.setValue(0);
-    if (currentPage == index) {
-      if (onFinish) {
-        onFinish(state);
-      }
-    }
-  }
-
-  const swipeText =
-    content?.[current]?.swipeText || props.swipeText || 'Swipe Up';
-
-  React.useEffect(() => {
-    if (onStorySeen && currentPage === index) {
-      onStorySeen({
-        user_id: userId,
-        user_image: profileImage,
-        user_name: profileName,
-        story: content[current],
-      });
-    }
-  }, [currentPage, index, onStorySeen, current]);
-
-  const extension = content[current]?.story_image?.split('.').pop()?.toLowerCase() || '';
-  const isVideo = ['mp4', 'avi', 'mov', 'wmv'].includes(extension);
-
-  console.log('content[current].story_image',content[current]);
-  
+  const avatarWrapperSize = avatarSize + 4;
 
   return (
-    <GestureRecognizer
-      key={key}
-      onSwipeUp={onSwipeUp}
-      onSwipeDown={onSwipeDown}
-      config={config}
-      style={[styles.container, storyContainerStyle]}
-    >
-      <SafeAreaView>
-        <View style={styles.backgroundContainer}>
-          {isVideo ? (
-            <Video source={{ uri: content[current].story_image }}
-            style={[styles.image, storyImageStyle]} onLoad={() => start()} />
-          ) :
-          (
-          <Image
-            onLoadEnd={() => start()}
-            source={{ uri: content[current].story_image }}
-            style={[styles.image, storyImageStyle]}
-          />
-          )}
-          {load && (
-            <View style={styles.spinnerContainer}>
-              <ActivityIndicator size="large" color={'white'} />
-            </View>
-          )}
-        </View>
-      </SafeAreaView>
-      <View style={styles.flexCol}>
-        <View
-          style={[styles.animationBarContainer, animationBarContainerStyle]}
+    <View style={styles.container}>
+      <TouchableOpacity
+        onPress={() => _handleItemPress(item)}
+        style={[
+          styles.avatarWrapper,
+          {
+            height: avatarWrapperSize,
+            width: avatarWrapperSize,
+          },
+          avatarWrapperStyle,
+          // !isPressed
+          //   ? {
+          //       borderColor: unPressedBorderColor ?? '#1877F2',
+          //     }
+          //   : {
+          //       borderColor: pressedBorderColor ?? 'grey',
+          //     },
+              !item?.isSeen
+              ? {
+                  borderColor: unPressedBorderColor ?? '#1877F2',
+                }
+              : {
+                  borderColor: pressedBorderColor ?? 'grey',
+                },
+        ]}
+      >
+        <Image
+          style={[
+            {
+              height: avatarSize,
+              width: avatarSize,
+              borderRadius: 100,
+            },
+            avatarImageStyle,
+          ]}
+          source={{ uri: item.user_image }}
+          defaultSource={Platform.OS === 'ios' ? DEFAULT_AVATAR : null}
+        />
+      </TouchableOpacity>
+      {showText && (
+        <Text
+          numberOfLines={1}
+          ellipsizeMode="tail"
+          style={[
+            {
+              width: avatarWrapperSize,
+              ...styles.text,
+              ...avatarTextStyle,
+            },
+            isPressed
+              ? { color: pressedAvatarTextColor || '#99A1BE' }
+              : { color: unPressedAvatarTextColor || '#99A1BE' },
+          ]}
         >
-          {content.map((index, key) => {
-            return (
-              <View
-                key={key}
-                style={[styles.animationBackground, unloadedAnimationBarStyle]}
-              >
-                <Animated.View
-                  style={[
-                    {
-                      flex: current == key ? progress : content[key].finish,
-                      height: 2,
-                      backgroundColor: 'white',
-                    },
-                    loadedAnimationBarStyle,
-                  ]}
-                />
-              </View>
-            );
-          })}
-        </View>
-        <View style={[styles.userContainer, storyUserContainerStyle]}>
-          <View style={styles.flexRowCenter}>
-            <Image
-              style={[styles.avatarImage, storyAvatarImageStyle]}
-              source={{ uri: profileImage }}
-            />
-            {typeof renderTextComponent === 'function' ? (
-              renderTextComponent({
-                item: content[current],
-                profileName,
-              })
-            ) : (
-              <Text style={styles.avatarText}>{profileName}</Text>
-            )}
-          </View>
-          <View style={styles.closeIconContainer}>
-            {typeof renderCloseComponent === 'function' ? (
-              renderCloseComponent({
-                onPress: onClosePress,
-                item: content[current],
-              })
-            ) : (
-              <TouchableOpacity
-                onPress={() => {
-                  if (onClosePress) {
-                    onClosePress();
-                  }
-                }}
-              >
-                <Text style={styles.whiteText}>X</Text>
-              </TouchableOpacity>
-            )}
-          </View>
-        </View>
-        <View style={styles.pressContainer}>
-          <TouchableWithoutFeedback
-            onPressIn={() => progress.stopAnimation()}
-            onLongPress={() => setPressed(true)}
-            onPressOut={() => {
-              setPressed(false);
-              startAnimation();
-            }}
-            onPress={() => {
-              if (!pressed && !load) {
-                previous();
-              }
-            }}
-          >
-            <View style={styles.flex} />
-          </TouchableWithoutFeedback>
-          <TouchableWithoutFeedback
-            onPressIn={() => progress.stopAnimation()}
-            onLongPress={() => setPressed(true)}
-            onPressOut={() => {
-              setPressed(false);
-              startAnimation();
-            }}
-            onPress={() => {
-              if (!pressed && !load) {
-                next();
-              }
-            }}
-          >
-            <View style={styles.flex} />
-          </TouchableWithoutFeedback>
-        </View>
-      </View>
-      {typeof renderSwipeUpComponent === 'function' ? (
-        renderSwipeUpComponent({
-          onPress: onSwipeUp,
-          item: content[current],
-        })
-      ) : (
-        <TouchableOpacity
-          activeOpacity={1}
-          onPress={onSwipeUp}
-          style={styles.swipeUpBtn}
-        >
-          <Text style={styles.swipeText}></Text>
-          <Text style={styles.swipeText}>{swipeText}</Text>
-        </TouchableOpacity>
+          {user?.id === item?.user_id ? 'Your Story' : item.user_name}
+        </Text>
       )}
-    </GestureRecognizer>
+    </View>
   );
 };
 
-export default StoryListItem;
-
-StoryListItem.defaultProps = {
-  duration: 10000,
-};
+export default StoryCircleListItem;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#000',
+    marginVertical: 5,
+    marginRight: 10,
   },
-  flex: {
-    flex: 1,
-  },
-  flexCol: {
-    flex: 1,
-    flexDirection: 'column',
-  },
-  flexRowCenter: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  image: {
-    width: width,
-    height: height,
-    resizeMode: 'cover',
-  },
-  backgroundContainer: {
-    position: 'absolute',
-    top: 0,
-    bottom: 0,
-    left: 0,
-    right: 0,
-  },
-  spinnerContainer: {
-    zIndex: -100,
-    position: 'absolute',
+  avatarWrapper: {
+    borderWidth: 2,
     justifyContent: 'center',
-    backgroundColor: 'black',
+    alignItems: 'center',
     alignSelf: 'center',
-    width: width,
-    height: height,
-  },
-  animationBarContainer: {
-    flexDirection: 'row',
-    paddingTop: 10,
-    paddingHorizontal: 10,
-  },
-  animationBackground: {
-    height: 2,
-    flex: 1,
-    flexDirection: 'row',
-    backgroundColor: 'rgba(117, 117, 117, 0.5)',
-    marginHorizontal: 2,
-  },
-  userContainer: {
-    height: 50,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    paddingHorizontal: 15,
-  },
-  avatarImage: {
-    height: 30,
-    width: 30,
+    borderColor: 'red',
     borderRadius: 100,
+    height: 64,
+    width: 64,
   },
-  avatarText: {
-    fontWeight: 'bold',
-    color: 'white',
-    paddingLeft: 10,
-  },
-  closeIconContainer: {
+  text: {
+    marginTop: 3,
+    textAlign: 'center',
     alignItems: 'center',
-    justifyContent: 'center',
-    height: 50,
-    paddingHorizontal: 15,
-  },
-  pressContainer: {
-    flex: 1,
-    flexDirection: 'row',
-  },
-  swipeUpBtn: {
-    position: 'absolute',
-    right: 0,
-    left: 0,
-    alignItems: 'center',
-    bottom: Platform.OS == 'ios' ? 20 : 50,
-  },
-  whiteText: {
-    color: 'white',
-  },
-  swipeText: {
-    color: 'white',
-    marginTop: 5,
+    fontSize: 11,
   },
 });
